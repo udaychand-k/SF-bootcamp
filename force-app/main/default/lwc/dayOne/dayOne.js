ace86aa2850db334df19f106f83f967957dbc25b
udaychand-k <udaychand141@gmail.com>
2024-12-01 22:55:16 +0530

    lwc:spread and lwc:ref

--- force-app/main/default/lwc/dayOne/dayOne.js
+++ force-app/main/default/lwc/dayOne/dayOne.js
@@ -1,8 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class DayOne extends LightningElement {
     username='udaychand';
     usernamealias;
+    
+    @track products=[
+        {label:'bike',value:'bike'},
+        {label:'cycle',value:'cycle'},
+        {label:'car',value:'car'}
+]
+value=this.products[1].value;
+data={products:this.products, addProduct:this.addProduct.bind(this)}
+addProduct(obj){
+    debugger;
+    this.products=[...this.products,obj];
+    this.data={products:this.products, addProduct:this.addProduct.bind(this)};
+
+}
+handleChangeCombobox(event){
+    this.value=event.detail.value;
+
+}
     handleChange(event){
         this.usernamealias=event.target.value;
     }
--- force-app/main/default/lwc/dayTwo/dayTwo.js
+++ force-app/main/default/lwc/dayTwo/dayTwo.js
@@ -3,7 +3,27 @@ import { LightningElement, api } from 'lwc';
 export default class DayTwo extends LightningElement {
     @api alias='Hemanth';
     updateusername;
+    @api products;
+    @api addProduct;
+    inputValue;
+    // handleChangeinput(event){
+    //     this.inputValue=event.target.value;
+    // }
+    handleClick(){
+        
+            this.inputValue=this.refs.myinput.value;
 
+            
+         let obj={label:this.inputValue,value:this.inputValue}
+     
+        
+        if(this.addProduct && this.inputValue){
+            
+            this.addProduct(obj);
+        }
+        this.refs.myinput.value='';
+
+    }
     handleChange(event){
         let e=new CustomEvent(
             'updateusername',
