8f95952797bf674cf9bde42748b2dc717a7058ec
udaychand-k <udaychand141@gmail.com>
2024-12-07 17:18:11 +0530

    lifecycle hooks

--- force-app/main/default/lwc/dayFour/dayFour.js
+++ force-app/main/default/lwc/dayFour/dayFour.js
@@ -2,7 +2,9 @@ import { LightningElement,api } from 'lwc';
 console.log('***********');
 console.log('*************************************'+ Id);
 import Id from '@salesforce/user/Id';
-
+import signi from './signin.html';
+import signu from './signup.html';
+import home from './myname.html';
 console.log('*'+ Id);
 
 export default class DayFour extends LightningElement {
@@ -11,6 +13,9 @@ export default class DayFour extends LightningElement {
     objName;
     rec;
     uid;
+    isRegistration;
+    isLogin;
+    a;
     constructor(){
         super();
         this.uid=Id;
@@ -21,13 +26,52 @@ export default class DayFour extends LightningElement {
         this.objName=this.objectApiName;
         
     }
+    render(){
+        console.log('render callback');
+        debugger;
+        return  this.a=='sign up'?signu:this.a=='sign in'?signi:home;
+    }
     renderedCallback(){
         
     }
+
     disconnectedCallback(){
 
     }
     errorCallback(){
 
+    }
+    handleclick(event){
+        
+         this.a = event.target.label;
+         console.log('*********'+this.a);
+         //this.render();
+         debugger;
+        
+
+    }
+    handleSubmit(event){
+        console.log('handle submit');
+        console.log(event.target.value);
+
+    }
+    handleLoginAndSignUP(){
+        this.isLogin=!this.isLogin;
+        this.isRegistration=!this.isRegistration;
+        return this.isRegistration?signu:this.isLogin?signi:home;
+    }
+    handleback(event){
+        this.isRegistration=false;
+        this.isLogin=false;
+        return this.isRegistration?signu:this.isLogin?signi:home;
+
+    }
+    handleRegistration(){
+        this.isRegistration=true;
+       return this.isRegistration?signu:singi;
+    }
+    handleLogin(){
+        this.isLogin=true;
+        return this.isRegistration?signu:this.isLogin?signi:home;
     }
 }
